cc211dcbd8e231cdc51307981adede492252739d
RichardA <richardamin93@gmail.com>
2020-02-18 22:23:13 -0500

    third commit

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -7,11 +7,11 @@ let start_num = document.getElementById("start_num").value;
 let end_num =  document.getElementById("end_num").value;
 
 
-var fizzBuzz = function(start, end) {
+var fizzBuzz = function(start_num, end_num) {
 
     output = []
 
-    for(var i = start; i <= end; i++){
+    for(var i = start_num; i <= end_num; i++){
 
         if(i % 3 === 0 && i % 5 === 0){
            output.push("FizzBuzz");
